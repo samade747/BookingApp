5458af973e64cc90cb4dcf38d4f0128436127fde
samade747 <samad.e747@gmail.com>
2024-05-09 12:32:05 +0500

    06am 9 5 2024

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -19,16 +19,31 @@ const Navbar = () => {
     return (
         <div className='navbar'>
           <div className='navConatiner'>
-            <Link 
-            
-            
-            
+            <Link to='/' style={{color: 'inherit', textDecoration: 'none'}}>
+                <span className='logo'>Booking App</span>
+            </Link>
+            <div className='navItems'>
+
+            {user ? <div className='userInfoWrapper'>
+                <Avatar
+                    style={{backgroundColor: '#FFB6C1', color: 'black',}}>
+                    {userNameFirstLetter}
+                </Avatar>
+                    <div className='userInfo'>
+                        <h2>{user?.username}</h2>
+                        <p>{user?.email}</p>                                            
+                  </div>
+                </div> : <>
+                    <button className='navButton2'>Register</button>
+                    <button className='navButton2'>Login</button>
+                </>}
             </div>  
+            <span className='userIcon'>
+                <FontAwesomeIcon icon={faUser} />
+            </span>
         </div>
-
-
-
+        </div>                                     
     )
-
-    
 }
+
+export default Navbar
